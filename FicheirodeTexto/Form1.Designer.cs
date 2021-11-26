0f795c89a5b271e5d49c87589b3009ab59ca6b1d
tomasvara12 <josevaralonga22@gmail.com>
2021-11-26 11:40:28 +0000

    Adicionar arquivos de projeto.

---
+++ FicheirodeTexto/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace FicheirodeTexto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.txtCouteudo = new System.Windows.Forms.TextBox();
+            this.btnlimpar = new System.Windows.Forms.Button();
+            this.btnContar = new System.Windows.Forms.Button();
+            this.txtQuant = new System.Windows.Forms.TextBox();
+            this.btnsalvar = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.Location = new System.Drawing.Point(12, 35);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(171, 44);
+            this.btnAbrir.TabIndex = 0;
+            this.btnAbrir.Text = "Selecionar Ficheiro";
+            this.btnAbrir.UseVisualStyleBackColor = true;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // txtCouteudo
+            // 
+            this.txtCouteudo.Location = new System.Drawing.Point(12, 85);
+            this.txtCouteudo.Multiline = true;
+            this.txtCouteudo.Name = "txtCouteudo";
+            this.txtCouteudo.Size = new System.Drawing.Size(349, 303);
+            this.txtCouteudo.TabIndex = 2;
+            // 
+            // btnlimpar
+            // 
+            this.btnlimpar.Location = new System.Drawing.Point(189, 35);
+            this.btnlimpar.Name = "btnlimpar";
+            this.btnlimpar.Size = new System.Drawing.Size(172, 44);
+            this.btnlimpar.TabIndex = 3;
+            this.btnlimpar.Text = "Limpar";
+            this.btnlimpar.UseVisualStyleBackColor = true;
+            this.btnlimpar.Click += new System.EventHandler(this.btnlimpar_Click);
+            // 
+            // btnContar
+            // 
+            this.btnContar.Location = new System.Drawing.Point(424, 35);
+            this.btnContar.Name = "btnContar";
+            this.btnContar.Size = new System.Drawing.Size(172, 44);
+            this.btnContar.TabIndex = 4;
+            this.btnContar.Text = "Contar";
+            this.btnContar.UseVisualStyleBackColor = true;
+            this.btnContar.Click += new System.EventHandler(this.btnContar_Click);
+            // 
+            // txtQuant
+            // 
+            this.txtQuant.Location = new System.Drawing.Point(367, 85);
+            this.txtQuant.Multiline = true;
+            this.txtQuant.Name = "txtQuant";
+            this.txtQuant.Size = new System.Drawing.Size(285, 303);
+            this.txtQuant.TabIndex = 5;
+            this.txtQuant.TextChanged += new System.EventHandler(this.txtQuant_TextChanged);
+            // 
+            // btnsalvar
+            // 
+            this.btnsalvar.Location = new System.Drawing.Point(321, 404);
+            this.btnsalvar.Name = "btnsalvar";
+            this.btnsalvar.Size = new System.Drawing.Size(153, 41);
+            this.btnsalvar.TabIndex = 6;
+            this.btnsalvar.Text = "Salvar";
+            this.btnsalvar.UseVisualStyleBackColor = true;
+            this.btnsalvar.Click += new System.EventHandler(this.btnsalvar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(480, 404);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(172, 41);
+            this.btnSair.TabIndex = 7;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 464);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnsalvar);
+            this.Controls.Add(this.txtQuant);
+            this.Controls.Add(this.btnContar);
+            this.Controls.Add(this.btnlimpar);
+            this.Controls.Add(this.txtCouteudo);
+            this.Controls.Add(this.btnAbrir);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.TextBox txtCouteudo;
+        private System.Windows.Forms.Button btnlimpar;
+        private System.Windows.Forms.Button btnContar;
+        private System.Windows.Forms.TextBox txtQuant;
+        private System.Windows.Forms.Button btnsalvar;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
+
---
+++ FicheirodeTexto/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security;
+using System.IO;
+using System.Text.RegularExpressions;
+
+
+namespace FicheirodeTexto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            string texto = "";
+
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                texto = File.ReadAllText(openFileDialog1.FileName);
+            txtCouteudo.Text = texto.ToString();
+
+
+
+        }
+
+        private void btnlimpar_Click(object sender, EventArgs e)
+        {
+            txtCouteudo.Clear();
+        }
+
+        private void btnContar_Click(object sender, EventArgs e)
+        {
+            // |-----------------------------------------------------------------------|
+            // Contar palavras
+            string texto = txtCouteudo.Text;
+
+            char[] separator = { ' ' };
+            texto = texto.Replace(Environment.NewLine, " ");
+
+            int wordsCount = texto.Split(separator, StringSplitOptions.RemoveEmptyEntries).Length;
+            int quantPlv = wordsCount;
+
+            // |-----------------------------------------------------------------------|
+            // Contar Caracteres 
+
+            int quantCrc = 0;
+            int i;
+            for (i = 0; i < texto.Length; i++)
+            {
+                quantCrc++;
+            }
+
+            // |-----------------------------------------------------------------------|
+            // Contar Paragrafo
+
+            string textoPrf = txtCouteudo.Text.Trim();
+            
+            int quantPrf = 0;
+            int prf;
+            for (prf= 0; prf < textoPrf.Length; prf++)
+            {
+                if (textoPrf[prf] == '.' || textoPrf[prf] == '?' || textoPrf[prf] == '!')
+                {
+                    quantPrf++;
+                }
+            }
+           
+            // |-----------------------------------------------------------------------|
+
+            string quant = "O ficheiro contém " + quantPlv + " palavras, " +quantCrc+ " caracteres e " +quantPrf+ " parágrafos.";
+            txtQuant.Text = quant.ToString();
+        }
+
+        private void txtQuant_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnsalvar_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "txt files (.txt)|.txt";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                using (Stream s = File.Open(saveFileDialog1.FileName, FileMode.OpenOrCreate))
+                using (StreamWriter sw = new StreamWriter(s))
+                {
+                    sw.Write(txtCouteudo.Text);
+                }
+            }
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
